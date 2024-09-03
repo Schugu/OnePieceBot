0b54ebf9a96d51fee46fb5544e9d4a589d3034e0
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-09-03 04:00:05 -0300

    Add: Comando 'clear'

---
+++ commands/clear.js
@@ -0,0 +1,34 @@
+export default {
+  description: 'Elimina un número específico de mensajes o todos los mensajes si no se proporciona un número.',
+  run: async (message, args) => {
+    try {
+      if (args.length === 0) {
+        // Eliminar todos los mensajes en el canal
+        let fetched;
+        do {
+          fetched = await message.channel.messages.fetch({ limit: 100 });
+          await message.channel.bulkDelete(fetched, true); // true para ignorar mensajes que no se pueden eliminar (como los más antiguos)
+        } while (fetched.size >= 2); // Continúa eliminando mientras haya mensajes
+
+        // Enviar un mensaje de confirmación después de eliminar los mensajes
+        await message.channel.send('Se han eliminado todos los mensajes en este canal.');
+      } else {
+        const amount = parseInt(args[0]);
+
+        if (!amount || amount < 1 || amount > 100) {
+          return message.reply('Por favor, proporciona un número entre 1 y 100.');
+        }
+
+        // Eliminar una cantidad específica de mensajes
+        const fetched = await message.channel.messages.fetch({ limit: amount + 1 });
+        await message.channel.bulkDelete(fetched, true);
+
+        // Enviar un mensaje de confirmación después de eliminar los mensajes
+        await message.channel.send(`Se han eliminado ${amount} mensajes.`);
+      }
+    } catch (error) {
+      console.error('Error al eliminar mensajes:', error);
+      message.reply('Hubo un problema al intentar eliminar los mensajes.');
+    }
+  }
+};
--- index.js
+++ index.js
@@ -15,18 +15,16 @@ client.on(Events.MessageCreate, async (message) => {
   if (message.author.bot) return;
   if (!message.content.startsWith('!')) return;
 
-  const args = message.content.slice(1).split(' '); 
-  const commandName = args[0].toLowerCase(); 
+  const args = message.content.slice(1).split(' ');
+  const commandName = args[0].toLowerCase();
 
   try {
     const command = await import(`./commands/${commandName}.js`);
-    command.default.run(message, args.slice(1)); // Pasar los argumentos restantes a la función `run`
+    command.default.run(message, args.slice(1));
   } catch (error) {
     console.log(`Ha ocurrido un error al utilizar el comando ${commandName}:`, error.message);
     message.reply(`No puedo ejecutar el comando ${commandName}. ¿Está escrito correctamente?`);
   }
 });
-
-
 
 client.login(process.env.BOT_TOKEN);
