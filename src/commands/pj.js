5906c459795266ffb8506fd5a61d1c179c991bbb
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-09-04 12:24:57 -0300

    Refactor: Forma en la que se muestran distintos resultados

--- src/commands/pj.js
+++ src/commands/pj.js
@@ -1,25 +1,60 @@
 import fetch from 'node-fetch';
+import { ActionRowBuilder, ButtonBuilder, EmbedBuilder, ButtonStyle } from 'discord.js';
 
-// Formatear para uno o pocos personajes 
+const PAGE_SIZE = 10; 
+const MAX_RETRIES = 3;
+
 const formatCharacterMessage = (character) => `
   **Nombre**: ${character.name || 'No disponible'}
-    **Altura**: ${character.size || 'No disponible'}
-    **Edad**: ${character.age || 'No disponible'}
-    **Recompensa**: ${character.bounty || 'No disponible'}
-    **Tripulación**: ${character.crew ? character.crew.name : 'No disponible'}
-    **Fruta del diablo**: ${character.fruit ? character.fruit.name : 'No disponible'}
-    **Ocupación**: ${character.job || 'No disponible'}
-    **Estado**: ${character.status || 'No disponible'}
+  **Altura**: ${character.size || 'No disponible'}
+  **Edad**: ${character.age || 'No disponible'}
+  **Recompensa**: ${character.bounty || 'No disponible'}
+  **Tripulación**: ${character.crew ? character.crew.name : 'No disponible'}
+  **Fruta del diablo**: ${character.fruit ? character.fruit.name : 'No disponible'}
+  **Ocupación**: ${character.job || 'No disponible'}
+  **Estado**: ${character.status || 'No disponible'}
 `;
 
-// Formatear para muchos personajes
-const formatCharacterName = (character) => `**Nombre**: ${character.name || 'No disponible'}`;
+const formatIdNameChar = (character) => `**${character.id ?? 'X'}**: ${character.name || 'No disponible'}`;
 
-// Tamaño máximo del mensaje en caracteres
-const MAX_MESSAGE_LENGTH = 2000;
+const createDetailedEmbed = (title, characters) => new EmbedBuilder()
+  .setTitle(title)
+  .setColor('#0099ff')
+  .setDescription(characters.map(formatCharacterMessage).join('\n'));
 
-// Tamaño máximo de los resultados antes de simplificar la información
-const DETAIL_THRESHOLD = 10; // Muestra detalles completos hasta 10 personajes
+const generateNameOnlyEmbed = (title, characters, page, totalPages) => {
+  const start = page * PAGE_SIZE;
+  const end = start + PAGE_SIZE;
+  const charactersPage = characters.slice(start, end);
+
+  return new EmbedBuilder()
+    .setTitle(title)
+    .setColor('#0099ff')
+    .setDescription(
+      charactersPage.map(formatIdNameChar).join('\n')
+    )
+    .setFooter({ text: `Página ${page + 1} de ${totalPages}` });
+};
+
+const fetchCharacterData = async (url, retries = MAX_RETRIES) => {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      console.error('Error en la respuesta:', response.status, response.statusText);
+      throw new Error('No se pudo obtener la información del personaje.');
+    }
+
+    return await response.json();
+  } catch (error) {
+    if (retries > 0) {
+      console.warn('Reintentando solicitud...', retries);
+      return fetchCharacterData(url, retries - 1);
+    } else {
+      throw error;
+    }
+  }
+};
 
 export default {
   description: 'Busca información sobre un personaje',
@@ -32,52 +67,59 @@ export default {
 
     try {
       const url = `https://api.api-onepiece.com/v2/characters/en/search?name=${encodeURIComponent(characterName)}`;
-
-      const response = await fetch(url);
-
-      if (!response.ok) {
-        console.error('Error en la respuesta:', response.status, response.statusText);
-        throw new Error('No se pudo obtener la información del personaje.');
-      }
-
-      const characterData = await response.json();
+      const characterData = await fetchCharacterData(url);
 
       if (!characterData || characterData.length === 0) {
         return message.reply('No se encontró información para el personaje especificado.');
       }
 
-      let replyMessage;
-
-      if (characterData.length > 10) {
-        replyMessage = `Se encontraron ${characterData.length} personajes con el nombre "${characterName}". Para ver más detalles, por favor, sé más específico en tu búsqueda.`;
-      } else if (characterData.length > DETAIL_THRESHOLD) {
-        replyMessage = `Se encontraron ${characterData.length} personajes con el nombre "${characterName}":\n\n${characterData.slice(0, DETAIL_THRESHOLD).map(formatCharacterName).join('\n\n')}\n\n(Sólo se muestran los primeros ${DETAIL_THRESHOLD} resultados.)`;
+      if (characterData.length === 1) {
+        const character = characterData[0];
+        await message.reply({ embeds: [createDetailedEmbed(`Información del personaje: ${character.name}`, [character])] });
+      } else if (characterData.length <= 10) {
+        await message.reply({ embeds: [createDetailedEmbed(`Resultados para "${characterName}"`, characterData)] });
       } else {
-        replyMessage = characterData.map(formatCharacterMessage).join(' ');
-      }
+        let currentPage = 0;
+        const totalPages = Math.ceil(characterData.length / PAGE_SIZE);
 
-      if (replyMessage.length > MAX_MESSAGE_LENGTH) {
-        let currentMessage = '';
-        const messages = [];
+        const createButtonRow = () => new ActionRowBuilder().addComponents(
+          new ButtonBuilder()
+            .setCustomId('prev')
+            .setLabel('⬅️ Anterior')
+            .setStyle(ButtonStyle.Primary)
+            .setDisabled(currentPage === 0),
+          new ButtonBuilder()
+            .setCustomId('next')
+            .setLabel('➡️ Siguiente')
+            .setStyle(ButtonStyle.Primary)
+            .setDisabled(currentPage === totalPages - 1)
+        );
 
-        const lines = replyMessage.split('\n');
-        for (const line of lines) {
-          if ((currentMessage + line).length > MAX_MESSAGE_LENGTH) {
-            messages.push(currentMessage);
-            currentMessage = line + '\n';
-          } else {
-            currentMessage += line + '\n';
+        const embedMessage = await message.channel.send({
+          embeds: [generateNameOnlyEmbed(`Resultados para "${characterName}"`, characterData, currentPage, totalPages)],
+          components: [createButtonRow()],
+        });
+
+        const collector = embedMessage.createMessageComponentCollector({ time: 60000 });
+
+        collector.on('collect', async (interaction) => {
+          if (!interaction.isButton()) return;
+
+          if (interaction.customId === 'prev' && currentPage > 0) {
+            currentPage--;
+          } else if (interaction.customId === 'next' && currentPage < totalPages - 1) {
+            currentPage++;
           }
-        }
-        if (currentMessage) {
-          messages.push(currentMessage);
-        }
 
-        for (const msg of messages) {
-          await message.reply(msg);
-        }
-      } else {
-        message.reply(replyMessage);
+          await interaction.update({
+            embeds: [generateNameOnlyEmbed(`Resultados para "${characterName}"`, characterData, currentPage, totalPages)],
+            components: [createButtonRow()],
+          });
+        });
+
+        collector.on('end', () => {
+          embedMessage.edit({ components: [] });
+        });
       }
     } catch (error) {
       console.error('Error al obtener información:', error);
